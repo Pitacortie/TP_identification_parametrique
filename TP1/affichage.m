52f3a2f9f8876d909c21e4194cec3786e4bf6fea
Antoine El Kahi <ton_email_github@example.com>
2026-09-11 17:53:24 +0200

    update

--- TP1/affichage.m
+++ TP1/affichage.m
@@ -2,40 +2,40 @@ clear;
 clc;
 close all;
 
-definit_param;
-simule_systeme;
+run("definit_param.m");
+run("simule_systeme.m");
 
-figure
+figure;
 
 subplot(3,1,1)
-plot(t, alpha)
+plot(t, alpha, 'LineWidth', 1.2)
 hold on
-plot(t, beta)
-plot(t, gamma)
+plot(t, beta, 'LineWidth', 1.2)
+plot(t, gamma, 'LineWidth', 1.2)
 grid on
-title('Positions mesurees')
+title('Positions')
 xlabel('Temps (s)')
 ylabel('Position (m)')
-legend('alpha','beta','gamma')
+legend('alpha', 'beta', 'gamma')
 
 subplot(3,1,2)
-plot(t, vit_alpha)
+plot(t, vit_alpha, 'LineWidth', 1.2)
 hold on
-plot(t, vit_beta)
-plot(t, vit_gamma)
+plot(t, vit_beta, 'LineWidth', 1.2)
+plot(t, vit_gamma, 'LineWidth', 1.2)
 grid on
-title('Vitesses mesurees')
+title('Vitesses')
 xlabel('Temps (s)')
 ylabel('Vitesse (m/s)')
-legend('vit alpha','vit beta','vit gamma')
+legend('vit alpha', 'vit beta', 'vit gamma')
 
 subplot(3,1,3)
-plot(t, acc_alpha)
+plot(t, acc_alpha, 'LineWidth', 1.2)
 hold on
-plot(t, acc_beta)
-plot(t, acc_gamma)
+plot(t, acc_beta, 'LineWidth', 1.2)
+plot(t, acc_gamma, 'LineWidth', 1.2)
 grid on
-title('Accelerations mesurees')
+title('Accelerations')
 xlabel('Temps (s)')
 ylabel('Acceleration (m/s^2)')
-legend('acc alpha','acc beta','acc gamma')
+legend('acc alpha', 'acc beta', 'acc gamma')
